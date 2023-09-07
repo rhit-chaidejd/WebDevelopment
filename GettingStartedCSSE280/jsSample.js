fac9ef8a5001be97948e864bd3d0bd9b1cdab0a0
rhit-chaidejd <chaidejd@rose-hulman.edu>
2023-09-07 13:37:24 -0400

    First Commit

---
+++ GettingStartedCSSE280/jsSample.js
@@ -0,0 +1,5 @@
+console.log("----------------------");
+console.log('Hello World!');
+console.log("Rise & Shine!");
+console.log("Ready for a new day!");
+console.log("----------------------");
---
+++ GettingStartedCSSE280/pythonSample.py
@@ -0,0 +1,5 @@
+print("----------------------------------")
+print("Python has entered the building.")
+print("Let the party start!")
+print("----------------------------------")
+
