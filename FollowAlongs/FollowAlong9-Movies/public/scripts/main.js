0edb59761fc194c5333d09e34aed9a37ff07bd09
rhit-chaidejd <chaidejd@rose-hulman.edu>
2023-10-24 14:29:38 -0400

    FollowAlong9

---
+++ FollowAlongs/FollowAlong9-Movies/public/scripts/main.js
@@ -0,0 +1,117 @@
+// Namespace
+var rhit = rhit || {};
+
+rhit.FB_COLLECTION_MOVIEQUOTE = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuotesManager = null;
+rhit.fbSingleQuoteManager = null;
+
+const db = firebase.firestore();
+db.collecion("cities");
+
+const query = firebase.firestore().collection('myCollection');
+
+query.get().then(querySnapshot => {
+    if (querySnapshot.empty) {
+        console.log('No matching documents.');
+        return;
+    }
+
+    querySnapshot.forEach(documentSnapshot => {
+        console.log(documentSnapshot.data());
+    });
+});
+
+
+rhit.FbSingleQuoteManager = class {
+    constructor(movieQuoteId) {
+    this._documentSnapshot = {};
+    this._unsubscribe = null;
+    this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTE).doc(movieQuoteId);
+    }
+    beginListening(changeListener) {
+    this._unsubscribe = this._ref.onSnapshot((doc) => {
+    if (doc.exists) {
+    console.log("Document data:", doc.data());
+    this._documentSnapshot = doc;
+    changeListener();
+    } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+    //window.location.href = "/";
+    }
+    });
+    }
+    stopListening() {
+    this._unsubscribe();
+    }
+    update(quote, movie) {
+    this._ref.update({
+    [rhit.FB_KEY_QUOTE]: quote,
+    [rhit.FB_KEY_MOVIE]: movie,
+    [rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+    })
+    .then(() => {
+    console.log("Document successfully updated!");
+    })
+    .catch(function (error) {
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
+    });
+    }
+    
+    
+    delete() {
+    return this._ref.delete();
+    }
+    
+    
+    get quote() {
+    return this._documentSnapshot.get(rhit.FB_KEY_QUOTE);
+    }
+    
+    
+    get movie() {
+    return this._documentSnapshot.get(rhit.FB_KEY_MOVIE);
+    }
+    }
+
+rhit.main = function () {
+	console.log("Ready");
+	// Bare bones read...
+	const ref = firebase.firestore().collection("MovieQuotes");
+	ref.onSnapshot((querySnapshot) => {
+		console.log("MoiveQuotes...");
+		querySnapshot.forEach((document) => {
+			console.log(document.data());
+		});
+	});
+
+};
+
+rhit.main = function () {
+	console.log("Ready");
+	// Bare bones read...
+	// const ref = firebase.firestore().collection("MovieQuotes");
+	// ref.onSnapshot((querySnapshot) => {
+	// 	console.log("MoiveQuotes...");
+	// 	querySnapshot.forEach((document) => {
+	// 		console.log(document.data());
+	// 	});
+	// });
+
+    // // Bare bones create...
+	// ref.add({
+	// 	quote: "My first quote",
+	// 	movie: "My first movie",
+	// 	lastTouched: firebase.firestore.Timestamp.now(),
+	// });
+
+
+};
+
+
+
+    
